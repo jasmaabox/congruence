2ec6beaba4a7788c3666e4453e78c297779a0b5e
jasmaa <jasonmaa3955@gmail.com>
2019-12-21 11:13:52 -0500

    Add user auth

--- web/index.js
+++ web/index.js
@@ -1,26 +1,99 @@
 const express = require('express');
 const axios = require('axios');
 const qs = require('querystring');
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const { pool } = require('./config')
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const { pool } = require('./config');
+
+
+passport.use(new LocalStrategy(
+    (username, password, cb) => {
+        pool.query('SELECT * FROM users WHERE username=$1', [username],
+            (error, results) => {
+                if (!error) {
+                    if (results.rows.length > 0 && bcrypt.compareSync(password, results.rows[0].password)) {
+                        return cb(null, results.rows[0]);
+                    }
+                    else {
+                        return cb(null, false)
+                    }
+                }
+                else {
+                    return cb(error);
+                }
+            });
+    }
+));
+
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+    pool.query('SELECT * FROM users WHERE id=$1', [id], (error, results) => {
+        if (error) {
+            return cb(error);
+        }
+        cb(null, results.rows[0]);
+    });
+});
+
 
 const app = express();
 const port = 3000;
 
+// === Templating ===
 app.use('/static', express.static('static'));
 app.set('view engine', 'pug');
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+// === Middleware ===
+app.use(require('morgan')('combined'));
+app.use(require('body-parser').json());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('cors')());
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// === Routes ===
 app.get('/', (req, res) => {
     res.render('home', { title: 'Home', message: 'hi there' });
 });
+
 app.route('/login')
     .get((req, res) => {
         res.render('login', { title: 'Login' });
+    })
+    .post(passport.authenticate('local', { failureRedirect: '/login' }), (req, res) => {
+        res.send("wow success");
+    });
+
+app.route('/createAccount')
+    .get((req, res) => {
+        res.render('createAccount', { title: 'Create Account' });
+    })
+    .post((req, res) => {
+        const { username, password, confirm_password } = req.body;
+
+        if(password != confirm_password){
+            res.redirect('/createAccount');
+            return;
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+
+        // MAKE USERNAMES UNIQUE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hash], error => {
+            if (!error) {
+                res.redirect('/login');
+            }
+            else {
+                res.redirect('/createAccount');
+            }
+        });
     });
 
 
@@ -55,7 +128,7 @@ app.route('/dummy3')
             else {
                 res.json({ status: 'failure', message: error })
             }
-        })
+        });
     })
     .post((req, res) => {
         const { username, password } = req.body;
