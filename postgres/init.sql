5bbb027152ea49521c4a2e17f73a6cb2d84badf3
jasmaa <jasonmaa3955@gmail.com>
2020-01-09 23:05:02 -0500

    Start adding admin pagination

--- postgres/init.sql
+++ postgres/init.sql
@@ -13,6 +13,7 @@ CREATE TABLE users (
     verified_account BOOLEAN,
     user_level INTEGER /* 0:student, 1:instructor, 2:admin, 3:super admin */
 );
+CREATE INDEX n_idx ON users USING btree (id);
 
 CREATE TABLE courses (
     id SERIAL PRIMARY KEY,
@@ -53,3 +54,29 @@ INSERT INTO courses (course_name) VALUES ('Analysis of Algorithms');
 INSERT INTO courses (course_name) VALUES ('Software Design');
 INSERT INTO courses (course_name) VALUES ('AI with LISP');
 INSERT INTO courses (course_name) VALUES ('Graphics');
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('a', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'a@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('b', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'b@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('c', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'c@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('d', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'd@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('e', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'e@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('f', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'f@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('g', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'g@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('h', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'h@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('i', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'i@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('j', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'j@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('k', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'k@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('l', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'l@gmail.com', true, 0, 1);
+INSERT INTO users (username, password, email, verified_account, user_level, association_id)
+VALUES ('m', '$2a$10$siCgGUWF8SLY3faKiy4bSOq3D5upZ6UZaoswP7XWPW6lP9PqwCMBO', 'm@gmail.com', true, 0, 1);
--- web/controllers/admin.js
+++ web/controllers/admin.js
@@ -2,11 +2,11 @@
 // Admin routes
 
 const express = require('express');
-const axios = require('axios');
 const qs = require('querystring');
 const { pool } = require('../config');
 const { verifyLevel } = require('../middleware');
 
+const PAGE_COUNT = 5;
 const router = express.Router();
 
 router.get('/', verifyLevel(2), (req, res) => {
@@ -17,6 +17,32 @@ router.get('/', verifyLevel(2), (req, res) => {
 });
 
 router.get('/users', verifyLevel(2), (req, res) => {
+
+    const cursor = req.query.cursor && Number.isInteger(parseInt(req.query.cursor))
+        ? parseInt(req.query.cursor)
+        : 0;
+
+    pool.query(
+        `SELECT * FROM users WHERE association_id=$1 AND id>$2 ORDER BY id ASC LIMIT $3`,
+        [req.user.association_id, cursor * PAGE_COUNT, PAGE_COUNT],
+    )
+        .then((result) => {
+
+            // Prev and next
+            const prev = cursor == 0 ? 0 : cursor - 1;
+            const next = cursor + 1;
+
+            res.render('admin/users', {
+                title: 'Admin',
+                user: req.user,
+                users: result.rows,
+                prev_href: `/admin/users?cursor=${prev}`,
+                next_href: `/admin/users?cursor=${next}`,
+            });
+        })
+        .catch((error) => {
+            res.send(error);
+        })
 });
 
 router.get('/', verifyLevel(2), (req, res) => {
